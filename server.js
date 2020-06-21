cc70a29442a1258a6f136178b31d2e50a6e43572
Azkom <azkom@t-online.hu>
2020-06-21 11:29:55 +0200

    Use middleware and connect to mongoDB by mongoose

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors');
+//This is an example middleware
+// const logger = require('./middleware/logger');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
-// Route files
-const bootcamps = require('./routes/bootcamps');
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
+// Connect to database
+connectDB();
+
 
 const app = express();
+
+// Activate/use logger middleware
+//app.use(logger);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
