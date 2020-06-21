5b2b8aae357ad67b4c14cafac78cd5b41a7b3cdf
Azkom <azkom@t-online.hu>
2020-06-21 16:02:38 +0200

    Bootcamp CRUD functionality and errorHandling middleware

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
+// Error handler middleware
+const errorHandler = require('./middleware/error');
 //This is an example middleware
 // const logger = require('./middleware/logger');
 const morgan = require('morgan');
@@ -17,6 +19,9 @@ connectDB();
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
 // Activate/use logger middleware
 //app.use(logger);
 if (process.env.NODE_ENV === 'development') {
@@ -25,6 +30,7 @@ if (process.env.NODE_ENV === 'development') {
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
