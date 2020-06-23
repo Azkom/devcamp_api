f8d615ab390d1603785bd9d039d8357b4acebee7
Azkom <azkom@t-online.hu>
2020-06-23 13:16:48 +0200

    Initial setup of Authentication

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ dotenv.config({ path: './config/config.env' });
 // Route files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 // Connect to database
 connectDB();
@@ -39,6 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
